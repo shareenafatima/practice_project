6181e28a381383350adb7ad7a280befa8e4cd73c
shareena fatima <shareenafatima40@gmail.com>
2024-03-27 22:31:56 +0500

    adding sign out function

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -5,7 +5,18 @@
 # are not: uncommented lines are intended to protect your configuration from
 # breaking changes in upgrades (i.e., in the event that future versions of
 # Devise change the default values for those options).
-#
+class TurboFailureApp < Devise::FailureApp
+  def respond
+    if request_format == :turbo_stream
+      :redirect
+    else
+      super
+    end
+    end
+    def skip_format?
+      %w[html turbo_stream].include? request_format.to_s
+  end
+end
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
@@ -15,7 +26,11 @@ Devise.setup do |config|
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = 'df05f9bc97f0ebc7e6ed9ce7a7c0b5539d4209c91f411f6aa4694f62585ff342f54aac249357c80df882440d1e0987a2bc67c7ca33dd24fb44628e16818deb43'
-
+config.parent_controller = "TurboDeviseController"
+config.navigational_formats = ['*/*', :html, :turbo_stream]
+config.warden do |manager|
+  manager.failure_app = TurboFailureApp
+end
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
