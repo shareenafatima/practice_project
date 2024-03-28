70602b8d213978fbb486df661de70e49c4d173b8
shareena fatima <shareenafatima40@gmail.com>
2024-03-28 23:11:45 +0500

    creating chat box

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   validates :title, presence:true, length:{minimum:5, maximum:15}
   validates :body, presence:true, length:{minimum:10, maximum:1500}
+  belongs_to :user
+  has_many :comments, dependent: :destroy
 end
