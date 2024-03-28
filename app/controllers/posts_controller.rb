f973fae6b367a1640ecdecdda821d0e996935148
shareena fatima <shareenafatima40@gmail.com>
2024-03-28 23:53:16 +0500

    modified

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,8 @@ before_action :authenticate_user!, except: %i[show index]
 
   # GET /posts/1 or /posts/1.json
   def show
-    @post.update(views:@post.views + 1)
+    @post.update(views: @post.views + 1)
+    @comments = @post.comments.order(created_at: :desc)
   end
 
   # GET /posts/new
